4700f8c983495f60c7e659e35512c0490d5f0720
CeciCode <101422795+CeciCode@users.noreply.github.com>
2022-12-25 17:52:48 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 status= "";
+objects= [];
 function setup() {
-    canvas= createCanvas(480, 380);
+    canvas= createCanvas(480, 300);
     canvas.center();
     video= createCapture(VIDEO);
     video.hide();
@@ -16,4 +17,32 @@ function modelLoaded() {
 }
 function draw() {
     image(video, 0, 0, 480, 380);
+    if(status != "") {
+        objectDetector.detect(video, gotResult);
+        for(i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML= "Status: ojebetos decetidados";
+            fill("pink");
+            percent= floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("pink");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+        if(objects[i].label == object) {
+            video.stop();
+            objectDetector.detect(gotResult);
+            document.getElementById("object_detected").innerHTML= "Ojebeto detectado";
+        }
+        else {
+            document.getElementById("object_detected").innerHTML= "O ojebeto não compareceu a reunião 🍷 🗿";
+        }
+
+    }
+}
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects= results;
 }
